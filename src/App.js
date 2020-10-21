b74a326f8946fadccb878a93622469866e3bfb3b
nuel-ikwuoma <emmanuelikwuoma7@gmail.com>
2020-10-21 17:46:23 +0100

    added initial set of features to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {EmployeeList} from './components/employeeList'
+import {EmployeeForm} from './components/employeeForm'
+import {Selector} from './components/selector'
+// import logo from './logo.svg';
 import './App.css';
 
+import initData from './initialData';
+
 function App() {
+  // for some weird reasons, i put the initial app state here
+  // im a jnr dev, pls bear with me and refactor it, tnks!.
+  const [employeeData, setEmployeeData] = useState(initData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Selector>
+        <EmployeeList entries={employeeData} name='employeelist' />
+        <EmployeeForm
+          saveCallback={setEmployeeData}
+          entries={employeeData}
+          name='employeeform'
+        />
+      </Selector>
     </div>
   );
 }
---
+++ src/initialData.js
@@ -0,0 +1,22 @@
+const initData = {
+  employees: [
+    {
+      id: 1,
+      fname: 'yoma',
+      lname: 'karo',
+      email: 'y@ko',
+      tel: '090',
+      city: 'ibadan',
+    },
+    {
+      id: 2,
+      fname: 'emma',
+      lname: 'kexk',
+      email: 'e@cm',
+      tel: '090',
+      city: 'lagos',
+    },
+  ],
+};
+
+export default initData;
